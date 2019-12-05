820fc6eb200521c95b0324784d642fda5645706c
guest19971220@gmail.com <guest19971220@gmail.com>
2019-12-05 20:55:32 +0800

    12.5new files

---
+++ practice1205/PHPpractice.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP practice</title>
+</head>
+<body>
+    <?php
+     print "hello world";
+    ?>
+</body>
+</html>
