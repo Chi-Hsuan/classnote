bfc963890d27094f85b2172d628e9bc84bbb7452
guest19971220@gmail.com <guest19971220@gmail.com>
2019-12-05 21:59:03 +0800

    mod

--- practice1205/PHPpractice2.php
+++ practice1205/PHPpractice2.php
@@ -3,7 +3,7 @@ $name = Peter;
 $money = 100;
 $description = Can you give me some money?
 
-printf($name.<br />);
-echo"$description.";
-print"I can give you $money..";
+printf(.$name.<br />);
+echo".$description.";
+print"I can give you .$money..";
 ?>
