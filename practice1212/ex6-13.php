b70997419233f3de0e8210b6dbf25ddb2c079339
guest19971220@gmail.com <guest19971220@gmail.com>
2019-12-12 21:10:38 +0800

    mod

--- practice1212/ex6-13.php
+++ practice1212/ex6-13.php
@@ -1,5 +1,6 @@
 <?php
-for($i=2;$i<10;$i++){
+
+/*for($i=2;$i<10;$i++){
  for($j=1;$j<10;$j++){
      printf("%d*%d=%d \t", $i,$j,($i*$j));
  }
@@ -19,4 +20,29 @@ for ($i = 2; $i < 10; $i++){
   }
   
 } 
+*/?>
+
+<br/>
+
+<?php
+  while (true){
+    echo "這是第一層迴圈！<br />";
+    while (true){
+      echo "這是第二層迴圈！<br />";
+      break 2;
+    }
+  }
+  echo "己跳出二層迴圈！";
 ?>
+
+<br/>
+
+<?php
+ /*   while($i <= 10){
+      if ( $i == 5) {
+        echo "略過 \$i = $i <br />";
+        continue;
+      }
+      echo $i."<br />";
+    }
+ */ ?>
