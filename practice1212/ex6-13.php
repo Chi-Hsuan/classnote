222c4362f3bc8c5a3b41655a46f256beaba80427
guest19971220@gmail.com <guest19971220@gmail.com>
2019-12-12 21:14:20 +0800

    mod

--- practice1212/ex6-13.php
+++ practice1212/ex6-13.php
@@ -38,11 +38,11 @@ for ($i = 2; $i < 10; $i++){
 <br/>
 
 <?php
- /*   while($i <= 10){
+    while($i <= 10){
       if ( $i == 5) {
         echo "略過 \$i = $i <br />";
         continue;
       }
       echo $i."<br />";
     }
- */ ?>
+  ?>
