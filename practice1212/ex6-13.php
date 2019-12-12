9118ab157418a4c6afef3771e88d59c683275348
guest19971220@gmail.com <guest19971220@gmail.com>
2019-12-12 20:40:59 +0800

    mod

--- practice1212/ex6-13.php
+++ practice1212/ex6-13.php
@@ -1,19 +1,22 @@
 <?php
 for($i=2;$i<10;$i++){
  for($j=1;$j<10;$j++){
-     printf("%d*%d=%d \t", $i,$j.($i*$j));
+     printf("%d*%d=%d \t", $i,$j,($i*$j));
  }
  printf("\n");
 }
 
 ?>
 
+<br/>
+
 <?php
 //九九乘法表
 for ($i = 2; $i < 10; $i++){
   for ($j = 1; $j < 10; $j++){
     printf("%d*%d=%d \t",$i,$j,($i * $j));
+    printf("\n");
   }
-  printf("\n");
+  
 } 
 ?>
