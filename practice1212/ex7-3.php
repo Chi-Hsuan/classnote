bfc4e831949c24da696f862bbd18af8f80041c21
guest19971220@gmail.com <guest19971220@gmail.com>
2019-12-12 22:14:44 +0800

    mod

--- practice1212/ex7-3.php
+++ practice1212/ex7-3.php
@@ -3,7 +3,7 @@ function tabble99($i=2,$j=1){
     if($j>9){
         $i++;
         $j=1;
-        echo"<br/>";
+        echo "<br/>";
     }
     if($i<=9){
         printf("%d*%d=%d \t", $i,$j,($i*$j));
