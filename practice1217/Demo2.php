c3f7298c03413b542a273122c1ea32c8d5be6e5d
guest19971220@gmail.com <guest19971220@gmail.com>
2019-12-17 20:40:44 +0800

    m

--- practice1217/Demo2.php
+++ practice1217/Demo2.php
@@ -1,10 +1,10 @@
 <?php
 include  "Shepherd.php";
-$mydog=new Shepherd("Peter","棕色","牧羊犬","10");
-echo $mydog->name. "<br />";
-echo $mydog->color. "<br />";
-echo $mydog->style. "<br />";
-echo $mydog->age. "<br />";
-echo $mydog->doWork(). "<br />";
-echo $mydog->dogRun()."<br />";
+$mydog= new Shepherd("Peter","棕色","牧羊犬","10");
+echo $mydog->name;
+echo $mydog->color;
+echo $mydog->style;
+echo $mydog->age;
+echo $mydog->doWork();
+echo $mydog->dogRun();
 ?>
