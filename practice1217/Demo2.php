840ce2973c92c14e24f9bee960d9a7bd7db659e1
guest19971220@gmail.com <guest19971220@gmail.com>
2019-12-17 20:36:03 +0800

    mod

--- practice1217/Demo2.php
+++ practice1217/Demo2.php
@@ -1,9 +1,10 @@
 <?php
 include  "Shepherd.php";
-$Mydog=new Shepherd("Peter","棕色","牧羊犬","10");
+$mydog=new Shepherd("Peter","棕色","牧羊犬","10");
 echo $mydog->name. "<br />";
 echo $mydog->color. "<br />";
 echo $mydog->style. "<br />";
 echo $mydog->age. "<br />";
 echo $mydog->doWork(). "<br />";
+echo $mydog->dogRun()."<br />";
 ?>
