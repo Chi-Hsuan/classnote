883a0f48a4dcad6bf84559451a4d71b3e7c7f71b
guest19971220@gmail.com <guest19971220@gmail.com>
2019-12-17 20:56:49 +0800

    test

--- practice1217/Demo2.php
+++ practice1217/Demo2.php
@@ -1,10 +1,10 @@
 <?php
 include  "Shepherd.php";
 $mydog= new Shepherd("Peter","棕色","牧羊犬","10");
- $mydog->name. "<br />";
- $mydog->color. "<br />";
- $mydog->style. "<br />";
- $mydog->age. "<br />";
- $mydog->doWork(). "<br />";
- $mydog->dogRun()."<br />";
+echo $mydog->name. "<br />";
+echo $mydog->color. "<br />";
+echo $mydog->style. "<br />";
+echo $mydog->age. "<br />";
+echo $mydog->doWork(). "<br />";
+echo $mydog->dogRun()."<br />";
 ?>
--- practice1217/Shepherd.php
+++ practice1217/Shepherd.php
@@ -3,17 +3,17 @@ include "Dog.php";
 class Shepherd extends Dog{
     protected $age;
 
-public function __construct($name,$color,$style,$age){
-    parent::__construct ($name,$color,$style);
-    $this -> age=$age;
-}
+    public function __construct($name,$color,$style,$age){
+        parent::__construct($name,$color,$style);
+        $this->age=$age;
+    }
 
-public function doWork(){
-    echo "趕羊..."."<br />";
-}
+    public function doWork(){
+        echo "趕羊..."."<br />";
+    }
 
-public function __destruct(){
-    echo "趕羊到家了";
-}
+    public function __destruct(){
+        echo "趕羊到家了";
+    }
 }
 ?>
