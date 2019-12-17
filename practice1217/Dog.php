28188959ee9f32642271530ae48f14bb8569571b
guest19971220@gmail.com <guest19971220@gmail.com>
2019-12-17 20:51:03 +0800

    mod

---
+++ practice1217/Dog.php
@@ -0,0 +1,25 @@
+<?php
+class Dog{
+    public $name;
+    public $color;
+    public $style;
+    
+    public function __construct($name,$color,$style){
+        $this->name = $name;
+        $this->color = $color;
+        $this->style = $style;
+    }
+
+    public function dogRun(){
+        echo "狗狗跑步中...";
+    }
+
+    public function dogBark(){
+        echo "狗叫...";
+    }
+
+    public function __destruct(){
+        echo "狗狗回家了...";
+    }
+}
+?>
