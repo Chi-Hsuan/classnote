86e7ffc3a1f84c03f884cef914a46341e16c0f36
guest19971220@gmail.com <guest19971220@gmail.com>
2019-12-19 20:41:02 +0800

    mod

---
+++ practice1219/Demo5.php
@@ -0,0 +1,31 @@
+<?php
+abstract class Animal{
+    public function move(){
+        echo "sleep";
+    }
+}
+
+class Dog extends Animal{
+    public function move(){
+        echo "run";
+    }
+}
+
+class Bird extends Animal{
+    public function move(){
+        echo "fly";
+    }
+}
+
+class Action{
+    public function actionMove(Animal $obj){
+        $obj->move();
+    }
+}
+
+$dog=new Dog;
+$bird=new Bird;
+$action=new Action;
+$action->ActionMove($dog);
+$action->ActionMOve($Bird);
+?>
