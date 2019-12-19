b8c06d4261ad12042c106c389098e512a9d53150
guest19971220@gmail.com <guest19971220@gmail.com>
2019-12-19 20:19:58 +0800

    mod

--- practice1219/Demo4.php
+++
@@ -1,4 +0,0 @@
-<?php
-include "Mathrate.php";
-echo Mathrate::Complexrate(1000,0.18,12,3);
-?>
--- practice1219/Mathrate.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Mathrate{
-    public static function Complexrate($principal,$yearRate,$period,$years){
-        $result = 0;
-        $result = $principal*pow((1 + ((float)$yearRate / $period)),($period*$years));
-        return $result;
-    }
-}
-?>
